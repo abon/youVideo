d48d73b48dd8675afb8872b583239f52adaedb60
abon <mr.abdullokh@gmail.com>
2019-07-23 05:28:24 +0500

    project set-up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur,
+          quis.
+        </h1>
+      </div>
+    );
+  }
+}
